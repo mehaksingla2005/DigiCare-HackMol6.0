1e2224841fd2625f97f65eb5e14a5359e59e286c
Mehak Singla <singlamehak2005@gmail.com>
2025-04-10 12:33:36 +0530

    Faq pages is added and added the routes after checking if the page is working or not backend integration is still left

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,83 @@
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import { useEffect, useState } from "react";
+import LoginButton from "./components/LoginButton";
+import FileUpload from "./components/Pdf";
+import AIAssistant from "./components/AiAssistant";
+import Explore from "./components/Explore";
+import AboutUsSection from "./components/AboutUs";
+import Footer from "./components/Footer";
+import FAQ from "./components/Faq.jsx";
 import Navbar from "./components/Navbar";
+import DoctorDashboard from "./components/DoctorDashboard";
+import Dashboard from "./components/Dashboard";
+import PatientDashboard from "./components/PatientDashboard";
+import PatientRegistration from "./components/PatientRegisteration.jsx";
+import DoctorRegistration from "./components/DoctorRegistration.jsx";
+import ProfilePage from './components/ProfilePage';
+import UserAvatar from "./components/UserAvatar.jsx";
+import Register from "./components/Register.jsx";
+import Login from "./components/Login.jsx";
+
+import UserProfile from "./components/UserProfile";
 
 
 function App() {
+  const { isAuthenticated, user, logout } = useAuth0();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userEmail, setUserEmail] = useState('');
+
+  useEffect(() => {
+    if (isAuthenticated && user) {
+      setIsLoggedIn(true);
+      setUserEmail(user.email);
+    } else {
+      setIsLoggedIn(false);
+      setUserEmail('');
+    }
+  }, [isAuthenticated, user]);
+
+  const handleLogin = (loggedIn, email) => {
+    setIsLoggedIn(loggedIn);
+    setUserEmail(email);
+  };
+
+  const handleLogout = () => {
+    logout({ returnTo: window.location.origin });
+    setIsLoggedIn(false);
+    setUserEmail('');
+  };
+
   return (
     <BrowserRouter>
       {/* Navbar appears on every page */}
       <Navbar isLoggedIn={isLoggedIn} userEmail={userEmail} onLogout={handleLogout} />
 
-
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/about" element={<AboutUsSection />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/2" element={<LoginButton />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/portal" element={<FileUpload />} />
+        <Route path="/image-analysis" element={<AIAssistant />} />
+        <Route path="/history" element={<AIAssistant />} />
+        
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/doctor-dashboard" element={<DoctorDashboard />} />
+        <Route path="/patient-dashboard" element={<PatientDashboard />} />
+        <Route path="/patient-registration" element={<PatientRegistration />} />
+        <Route path="/doctor-registration" element={<DoctorRegistration />} />
+        {/* <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/UserAvatar" element={<UserAvatar />} /> */}
+        <Route path="/profile" element={<UserProfile />} />
+      </Routes>
+      
+    </BrowserRouter>
   );
 }
 
---
+++ Frontend/src/components/HomePage.jsx
@@ -0,0 +1,27 @@
+
+import Description from './Description'
+import Explore from './Explore'
+ import AboutUsSection from './AboutUs'
+ import Footer from './Footer'
+import FAQ from './Faq.jsx';
+
+
+function HomePage() {
+  return (
+    // <>
+        <div className="app-container">   
+            <div className='content'>
+                <Description/>
+                <Explore/>
+                <AboutUsSection/>
+                <FAQ/>
+                <Footer/> 
+            </div>
+        </div>
+    // </>
+    
+ 
+  );
+}
+
+export default HomePage
