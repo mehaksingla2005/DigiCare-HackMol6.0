de63f6f6b36cad4cdd9c75d06bdf6297b5bfdf62
Mehak Singla <singlamehak2005@gmail.com>
2025-04-11 08:41:02 +0530

    cors error resolve

--- Backend/server.js
+++ Backend/server.js
@@ -20,7 +20,8 @@ mongoose.connection.on("disconnected", () => {
   console.log("MongoDB disconnected");
 });
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    // origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin:true,
     credentials: true
 }));
 app.use(express.json()); 
